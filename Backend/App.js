76930825a4d74c17c29e88804815ace7b8e01d40
Lowkik-Sai <lowkiksaipotnuru@gmail.com>
2024-02-08 20:24:19 +0530

    minor changes

--- Backend/App.js
+++ Backend/App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const mainRouter = require('../Backend/mainRoutes');
+const mainRouter = require('./mainRouter');
 
 app.use(express.json());
 app.use(cors());
--- Backend/mainRouter.js
+++ Backend/mainRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Get_Repo = require('../Backend/Routers/Get_Repo_Route');
-const Get_Access_Token = require('../Backend/Routers/Get_Access_Token_Router');
+const Get_Repo = require('./Routers/Get_Repo_Route');
+const Get_Access_Token = require('./Routers/Get_Access_Token_Router');
 
 router.use("/", Get_Repo);
 router.use("/", Get_Access_Token);
