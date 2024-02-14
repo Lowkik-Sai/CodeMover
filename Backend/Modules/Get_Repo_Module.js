f0a9d69c8213d373cf40e32119e04db07fe42814
Lowkik-Sai <lowkiksaipotnuru@gmail.com>
2024-02-14 21:52:40 +0530

    rateLimit check added simply

--- Backend/Modules/Get_Repo_Module.js
+++ Backend/Modules/Get_Repo_Module.js
@@ -2,11 +2,12 @@ const {Octokit} = require("@octokit/core")
 require("dotenv").config()
 
 
+
 let responseCode = 200;
 let responseBody = "";
 
 const Get_Repo_Module =  {
-    getRepo : async(Access_Token)=>{
+    getRepo : async()=>{
         try{
 
             const octokit = new Octokit({
@@ -16,9 +17,9 @@ const Get_Repo_Module =  {
             const api_response = await octokit.request('GET /user/repos',{
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28'
-                    
                 }
             })
+            console.log(api_response)
             const repo_names = api_response.data.map(obj => obj.name);
             responseBody = repo_names;
 
--- Backend/mainRouter.js
+++ Backend/mainRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const Get_Repo = require('./Routers/Get_Repo_Route');
 const Get_Access_Token = require('./Routers/Get_Access_Token_Router');
 const Login = require('./Routers/Login_Router');
@@ -8,11 +9,16 @@ const commit = require('./Routers/Commit_Route');
 const webTokenValidator = require('./Middleware/webTokenValidator');
 
 
-router.use("/", Get_Repo);
+//Middlewares
+const webTokenValidator = require('./Middleware/webTokenValidator');
+const rateLimitCheck = require('./Middleware/rateLimit')
+
+router.use("/", rateLimitCheck, Get_Repo);
 router.use("/restricted", webTokenValidator, Get_Access_Token);
 router.use("/", Login);
 router.use("/", Register);
 router.use("/",commit);
+router.use("/", rateLimitCheck, commit);
 
 
 module.exports = router;
