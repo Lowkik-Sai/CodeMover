1c509d47085d700e3e103c9aa375b0f254429ebb
Naganathan05 <umaiyalramesh@gmail.com>
2024-02-26 01:38:50 +0530

    MainPage Styles and Design Modified

---
+++ Frontend/JavaScript/Questionnaire.js
@@ -0,0 +1,15 @@
+const Questions = [
+    "Welcome to Code-Mover",
+    "What's the Data Structure Utilized by the Solution?",
+    "Enter the Question Name",
+    "In which Programming Language was the Solution Crafted?",
+    "What's the Time Complexity of the Solution?",
+    "What's the Space Complexity of the Solution?",
+    "Please Provide the Code for the Solution",
+    "Question Link [Optional]",
+    "Any Additional Notes to be Stored?",
+    "What Difficulty Level does the Question Belong To?",
+    "Please Enter 'confirm' if you're Ready to Proceed with the Commit"
+]
+
+module.exports = Questions;
--- Frontend/JavaScript/index.js
+++ Frontend/JavaScript/index.js
@@ -25,10 +25,18 @@ function sendDataToBackend(data) {
 document.addEventListener("DOMContentLoaded", function() {
     let countOfQns = 0;
     const globalQuestions = [
-        "Q1",
-        "Q2",
-        "Q3"
-    ];
+        "Welcome to Code-Mover",
+        "What's the Data Structure Utilized by the Solution?",
+        "Enter the Question Name",
+        "In which Programming Language was the Solution Crafted?",
+        "What's the Time Complexity of the Solution?",
+        "What's the Space Complexity of the Solution?",
+        "Please Provide the Code for the Solution",
+        "Question Link [Optional]",
+        "Any Additional Notes to be Stored?",
+        "What Difficulty Level does the Question Belong To?",
+        "Please Enter 'confirm' if you're Ready to Proceed with the Commit"
+    ]
 
     let answersReceived=[];
     let countOfAns = 0;
