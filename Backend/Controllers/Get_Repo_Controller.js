0fa03f4185dd4dea66b66e165cc1d82e3918d300
Lowkik Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2024-03-20 18:40:44 +0530

    Delete Backend directory

--- Backend/Controllers/Get_Repo_Controller.js
+++
@@ -1,9 +0,0 @@
-const Get_Repo_Module = require('../Modules/Get_Repo_Module');
-
-const Get_Repo_Controller = async(req, res) => {
-    const Access_Token = req.body.Access_Token;
-    const Get_Repo_response = await Get_Repo_Module.getRepo(Access_Token);
-    res.status(Get_Repo_response.responseCode).json(Get_Repo_response.responseBody);
-}
-
-module.exports = Get_Repo_Controller;
--- Backend/Controllers/Register_Controller.js
+++
@@ -1,9 +0,0 @@
-const Register_Module = require('../Modules/Register_Module');
-
-const Register_Controller = async(req, res) => {
-    const { User_Name, Password, Access_Token, Email_ID } = req.body;
-    const Register_response = await Register_Module(User_Name, Password, Access_Token, Email_ID);
-    res.status(Register_response.responseCode).json({message: Register_response.responseBody});
-}
-
-module.exports = Register_Controller;
