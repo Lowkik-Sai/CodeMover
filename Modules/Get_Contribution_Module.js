d081fe09c1a3470ca022c0687c0bff29e7a31dc9
Lowkik Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2024-03-20 18:50:07 +0530

    Add files via upload

---
+++ Modules/Get_Contribution_Module.js
@@ -0,0 +1,44 @@
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
+
+var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+let responseCode = 200;
+let responseBody = "";
+
+const Get_Contribution = async(User_Name) => {
+    var params = {
+        Key: {
+            "User_Name": {
+                "S": User_Name
+            }
+        },
+        TableName: "Auth"
+    };
+
+    ddb.getItem(params, function(err, data) {
+        if(err){
+            console.log(err);
+            responseCode = 100;
+            responseBody = err;
+        }
+        else{
+            console.log(data);
+            responseBody = data.Item.Total_Contributions.N;
+        }
+    });
+
+    const response = {
+        responseCode,
+        responseBody
+    };
+    return response;
+}
+
+module.exports = Get_Contribution;
