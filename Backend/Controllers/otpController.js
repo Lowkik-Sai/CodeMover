3ad19aeaa7c4615159bfeda95027826001c6b096
Lowkik-Sai <lowkiksaipotnuru@gmail.com>
2024-03-24 11:10:51 +0530

    Added new features and resolved minor bugs

--- Backend/Controllers/otpController.js
+++ Backend/Controllers/otpController.js
@@ -1,20 +1,25 @@
 const {otpModule,otpGenerator} = require("../Modules/otpModule");
 
-const otpController = async(req,res)=>{
-    const otpGenerate=await otpGenerator(req.params.User_Name);
-    if(otpGenerate.responseCode==200){
-        // const otpResponse = otpModule(req)
-        // if(otpResponse.responseCode == 200){
-        //     res.status(200).json(otpResponse.responseBody);
-        // }
-        // else{
-        //     res.status(100).json(otpResponse.responseBody);
-        // }
-        res.status(200).json(otpGenerate.responseBody)
-    }else{
-        //Email not exists or OTP sending failed
-        res.status(404).json(otpGenerate.responseBody)
+const otpController={
+    otpGenerate:async(req,res)=>{
+        const otpGenerate=await otpGenerator(req.params.User_Name);
+        if(otpGenerate.responseCode==200){
+            res.status(200).json(otpGenerate.responseBody)
+        }else{
+            //Email not exists or OTP sending failed
+            res.status(404).json(otpGenerate.responseBody)
+        }
+    },
+    otpVerify:async(req,res)=>{
+        const otpResponse = await otpModule(req)
+        if(otpResponse.responseCode == 200){
+            res.status(200).json(otpResponse.responseBody);
+        }
+        else{
+            res.status(100).json(otpResponse.responseBody);
+        }
     }
 }
+
 
 module.exports= otpController;
