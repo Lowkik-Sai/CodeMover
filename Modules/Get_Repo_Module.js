8dce458d17c1bc8e0b123485352b886937ad7bad
Naganathan <umaiyalramesh@gmail.com>
2024-09-30 17:33:55 +0530

    Commit Module Bug Fix

--- Modules/Get_Repo_Module.js
+++ Modules/Get_Repo_Module.js
@@ -1,24 +1,36 @@
 const {Octokit} = require("@octokit/core")
 require("dotenv").config()
-const Access_Token = process.env.gitToken;
+// Bug Found :)
+// const Access_Token = process.env.gitToken;
 let responseCode = 200;
 let responseBody = "";
 
 const Get_Repo_Module =  {
-    getRepo : async()=>{
-        try{
+    getRepo : async(Access_Token)=>{
 
+        Access_Token = String(Access_Token).trim(); 
+        if (Access_Token === '') {
+            console.error("Invalid Access Token provided.");
+            return {
+                responseCode: 400,
+                responseBody: "Invalid Access Token"
+            };
+        }
+        
+        try{
+            // console.log("Inside Get Repo Module: ", Access_Token);
             const octokit = new Octokit({
                 auth: Access_Token
             })
-
+            
+            // console.log("API RESPONSE STARTS HERE !!");
             const api_response = await octokit.request('GET /user/repos',{
                 headers: {
                     'X-GitHub-Api-Version': '2022-11-28'
                 }
             })
-            console.log(api_response)
             const repo_names = api_response.data.map(obj => obj.name);
+            // console.log("Repo Names: ", repo_names);
             responseBody = repo_names;
 
         }catch (error) {
