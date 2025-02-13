139169e9b79abbda36a21e984eca408201bf7707
Lowkik Sai <lowkiksaipotnuru@gmail.com>
2025-02-13 16:05:40 +0530

    getRepoRoute modified

--- Controllers/Get_Repo_Controller.js
+++ Controllers/Get_Repo_Controller.js
@@ -1,12 +1,23 @@
 const Get_Repo_Module = require('../Modules/Get_Repo_Module');
 
 const Get_Repo_Controller = async (req, res) => {
-    const Access_Token = req.query.Access_Token;
-    const Get_Repo_response = await Get_Repo_Module.getRepo(Access_Token);
-    
-    // console.log("Log from Controller..");
-    // console.log(Get_Repo_response.responseBody);
-    return res.status(Get_Repo_response.responseCode).json({ Repos: Get_Repo_response.responseBody });
-}
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({ error: "Unauthorized: No token provided" });
+        }
+
+        const Access_Token = authHeader.split(" ")[1];
+
+        const Get_Repo_response = await Get_Repo_Module.getRepo(Access_Token);
+
+        return res.status(Get_Repo_response.responseCode).json({ Repos: Get_Repo_response.responseBody });
+    } catch (error) {
+        console.error("Error in Get_Repo_Controller:", error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
 
 module.exports = Get_Repo_Controller;
