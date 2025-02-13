a8a6e9968a030e51bea2b1f762c73d5096cbc377
Lowkik Sai <lowkiksaipotnuru@gmail.com>
2025-02-13 16:21:45 +0530

    Fixed Bug in GetRepoController and WebTokenValidator options

--- Controllers/Get_Repo_Controller.js
+++ Controllers/Get_Repo_Controller.js
@@ -2,13 +2,11 @@ const Get_Repo_Module = require('../Modules/Get_Repo_Module');
 
 const Get_Repo_Controller = async (req, res) => {
     try {
-        const authHeader = req.headers.authorization;
+        const Access_Token = req.headers["x-access-token"];
 
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            return res.status(401).json({ error: "Unauthorized: No token provided" });
+        if (!Access_Token) {
+            return res.status(401).json({ error: "Unauthorized: No Access token provided" });
         }
-
-        const Access_Token = authHeader.split(" ")[1];
 
         const Get_Repo_response = await Get_Repo_Module.getRepo(Access_Token);
 
--- Middleware/webTokenValidator.js
+++ Middleware/webTokenValidator.js
@@ -1,23 +1,23 @@
 const jwt = require('jsonwebtoken');
 
-async function tokenValidator(req, res, next){
-    const token = req.headers.authorization;
-    if(!token){
-        res.status(401).send({
-            "ERROR": "No Token. Warning."
-        });
-        return;
+async function tokenValidator(req, res, next) {
+    const jwtToken = req.headers.authorization;  // JWT Token
+    const accessToken = req.headers["x-access-token"]; // GitHub Access Token
+
+    if (!jwtToken) {
+        return res.status(401).json({ "ERROR": "No JWT Token. Warning." });
     }
-    
+
     try {
-        const decodedToken = jwt.verify(token, "my-32-character-ultra-secure-and-ultra-long-secret");
+        // Verify JWT Token
+        const decodedToken = jwt.verify(jwtToken.split(" ")[1], "my-32-character-ultra-secure-and-ultra-long-secret");
         req.User_Name = decodedToken.User_Name;
+        req.Access_Token = accessToken; // Store Access Token in request object for further use
         next();
     } catch (error) {
-        res.status(401).send({
-            "ERROR": "Unauthorized access. Warning."
-        });
+        return res.status(401).json({ "ERROR": "Unauthorized access. Warning." });
     }
 }
+
 
 module.exports = tokenValidator;
