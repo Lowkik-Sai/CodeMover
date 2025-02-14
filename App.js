fa6a341874b9332731d4b35555ef27f9c2dfa921
Lowkik Sai <64127668+Lowkik-Sai@users.noreply.github.com>
2025-02-14 10:24:58 +0530

    Updated CorsOpts

--- App.js
+++ App.js
@@ -26,7 +26,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(cors(corsOpts));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
