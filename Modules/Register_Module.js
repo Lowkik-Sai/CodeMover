2addd6d929e37a1bc67b5771e71330d02b3c6181
Lowkik Sai <lowkiksaipotnuru@gmail.com>
2025-05-02 08:52:09 +0530

    Fixed returning response

--- Modules/Register_Module.js
+++ Modules/Register_Module.js
@@ -24,52 +24,53 @@ let response = {
 const Register_Module = {
     setAccountCreds : async(User_Name, Password, Access_Token, Email_ID) => {
         try {
-                // Check if a User already exists with the given User_Name
-                const data = await ddb.getItem({
-                    Key: {
-                        "User_Name": { "S": User_Name }
-                    },
-                    TableName: "Auth"
-                }).promise();
-        
-                if (data.Item && data.Item.User_Name.S === User_Name) {
-                    response.responseCode = 420;
-                    response.responseBody = "Already Exists, Login Instead";
-                    return response;
-                }
-        
-        
-            // Insert User Details to DynamoDB
-            const hashedPassword = crypto.createHash('sha256').update(Password).digest('hex');
-        
-            var params = {
-                TableName: "Auth",
-                Item: {
-                    User_Name: { S: User_Name },
-                    Password: { S: hashedPassword },
-                    Access_Token: { S: Access_Token },
-                    Email: { S: Email_ID },
-                    Total_Contributions: { N: "0" },
-                    OTP: { S: "" }
-                }
-            };
-        
-            ddb.putItem(params, (err, data) => {
-                if(err){
-                    console.log(err);
-                    response.responseCode = 422;
-                    response.responseBody = "Failed to Add User to Database";
-                }
-                else{
-                    response.responseCode = 200;
-                    response.responseBody = "Successfully Added User to Database";
-                    return response;
-                }
+            // Check if a User already exists with the given User_Name
+            const data = await ddb.getItem({
+                Key: {
+                    "User_Name": { "S": User_Name }
+                },
+                TableName: "Auth"
             }).promise();
+        
+            if (data.Item && data.Item.User_Name.S === User_Name) {
+                response.responseCode = 420;
+                response.responseBody = "Already Exists, Login Instead";
+            }else{
+                // Insert User Details to DynamoDB
+                const hashedPassword = crypto.createHash('sha256').update(Password).digest('hex');
+            
+                var params = {
+                    TableName: "Auth",
+                    Item: {
+                        User_Name: { S: User_Name },
+                        Password: { S: hashedPassword },
+                        Access_Token: { S: Access_Token },
+                        Email: { S: Email_ID },
+                        Total_Contributions: { N: "0" },
+                        OTP: { S: "" }
+                    }
+                };
+            
+                ddb.putItem(params, (err, data) => {
+                    if(err){
+                        console.log(err);
+                        response.responseCode = 422;
+                        response.responseBody = "Failed to Add User to Database";
+                    }
+                    else{
+                        response.responseCode = 200;
+                        response.responseBody = "Successfully Added User to Database";
+                    }
+                }).promise();
+
+            }
+        
+        
             
         } catch (error) {
             response.responseCode = 500;
             response.responseBody = "Internal Server Error";
+        } finally{
             return response;
         }
 
@@ -130,7 +131,6 @@ const Register_Module = {
 
                 }
 
-                return response;
             }else if(type === "signup"){
                 const token = jwt.sign({ 
                                         User_Name: username,
@@ -143,13 +143,13 @@ const Register_Module = {
                 response.signupToken = token;
                 response.avatar_url = avatar_url; //In future, this can be used to display user's avatar in frontend
                 
-                return response;
             }
             
         } catch (error) {
             response.responseCode = 400;
             response.responseBody = "Failed to Authenticate with GitHub";
             console.error('Error:', error);
+        } finally {
             return response;
         }
     }
